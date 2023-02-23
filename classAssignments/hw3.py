c9039d99360d696f7bc0e3f13b3e5daa12f63814
Parker Bowling <parkerbowling@pop-os.localdomain>
2023-02-23 17:34:41 -0500

    finshed hw3

---
+++ classAssignments/hw3.py
@@ -0,0 +1,25 @@
+#Parker Bowling
+#hw3
+#BCPP481
+#This homework is on loops
+
+myList = [87, 97, 100, 100, 59, 98, 77, 79, 81, 85, 70]
+
+#define a function called "average" that takes in myList as an argument
+# 1. print the average in a string called "The average is X" -> where X will be the average you find
+#you are not allowed to use built in functions like sum()
+# 2. create a list that only contains even scores and print the list
+# 3. iterate through the list of scores and 
+#    if that score is above an 80 and an even number score -> print "This score is above 80 and even"
+#    if a score is below the average or is a 70 or lower -> print "Do better next time"
+#    if a score is a 100 print "Perfect!"
+# 4. In another loop, iterate through the loop and 
+#    print out each score if the score equals 100 print "Perfect Score!" and immediately break from the loop
+# 5. return 0 when you are done
+
+'''write code here (you should have a total of three separte loops (One loop for 1&2, One loop for 3, and One loop for 4))'''
+
+'''end of code'''        
+
+#make sure to call your function!
+
